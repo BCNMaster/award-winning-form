74e5566de9277cd00c7defc80291365f47099a18
BCNMaster <bottlechainnetwork@gmail.com>
2025-04-05 20:42:29 -0400

    Add App component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AwardWinningForm from './components/AwardWinningForm';
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen py-8">
+      <div className="max-w-4xl mx-auto px-4">
+        <h1 className="text-3xl font-bold text-center mb-8 text-blue-700">Website Project Builder</h1>
+        <AwardWinningForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
