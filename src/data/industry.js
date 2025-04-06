ffe1d2a692f7412e904436f74b9502c26b06bcd7
BCNMaster <bottlechainnetwork@gmail.com>
2025-04-05 22:01:35 -0400

    Fix industry data export to use default export

---
+++ src/data/industry.js
@@ -0,0 +1,27 @@
+const industryData = {
+  businessAges: [
+    { id: 1, name: '0-1 years' },
+    { id: 2, name: '1-3 years' },
+    { id: 3, name: '3-5 years' },
+    { id: 4, name: '5-10 years' },
+    { id: 5, name: '10+ years' }
+  ],
+  companySizes: [
+    { id: 1, name: '1-10 employees' },
+    { id: 2, name: '11-50 employees' },
+    { id: 3, name: '51-200 employees' },
+    { id: 4, name: '201-500 employees' },
+    { id: 5, name: '500+ employees' }
+  ],
+  industries: [
+    { id: 1, name: 'Technology' },
+    { id: 2, name: 'Finance' },
+    { id: 3, name: 'Healthcare' },
+    { id: 4, name: 'Education' },
+    { id: 5, name: 'Retail' },
+    { id: 6, name: 'Manufacturing' },
+    { id: 7, name: 'Other' }
+  ]
+};
+
+export default industryData;
